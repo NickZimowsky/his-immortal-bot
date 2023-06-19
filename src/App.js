7a178b8dc4dfb3e55d39c447ca0ea8f789febfbd
NickZimowsky <nick.zimowskygmail.com>
2023-06-19 10:00:17 +0300

    test-com

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
       <Routes>
         <Route index element={ <ProductList /> } />
-        <Route path='form' element={ <TForm />} />
+        <Route path='/form' element={ <TForm />} />
 
       </Routes>
       
